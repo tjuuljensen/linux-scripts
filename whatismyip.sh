ecc4122b7b8265abbb5e98e578c9adc9c2ef3291
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2021-08-14 15:11:53 +0200

    LOCALIP added. INSTALL executable flag set

--- whatismyip.sh
+++ whatismyip.sh
@@ -5,10 +5,15 @@
 # resolved ip address on OpenDNS and Google
 OPENDNSRESOLVEDIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 GOOGLERESOLVEDIP=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed 's/"//g')
+LOCALIP=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+
+# wanip curl method: echo $(curl -s ipinfo.io/ip )
+# localip=$(hostname -i|cut -f2 -d ' ')
 
 # compare results - if different, report both (this happens sometimes on anonymized services)
 if [ $OPENDNSRESOLVEDIP == $GOOGLERESOLVEDIP ] ; then
-  echo $OPENDNSRESOLVEDIP
+  echo "Public IP: " $OPENDNSRESOLVEDIP
+  echo "Private IP: " $LOCALIP
   exit 0
 else
   echo "OpenDNS resolved IP: " $OPENDNSRESOLVEDIP
